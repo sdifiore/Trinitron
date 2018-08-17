e48cf52f76f4212cbeea2000837146a3d836eb2a
Sergio Di Fiore <difiores@outlook.com>
2018-08-17 11:16:36 -0300

    Model Created

---
+++ Trinitron/Persistence/VegaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trinitron.Models;
+
+namespace Trinitron.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Make> Makes { get; set; }
+    }
+}
